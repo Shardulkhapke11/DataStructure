570124c1cc51a8ee377296d6be5788cf2d56b02e
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-07-15 00:14:35 +0530

    LinkedList Stack Queue

---
+++ src/Com/BridgeLabz/Programs/Stack.java
@@ -0,0 +1,60 @@
+package Com.BridgeLabz.Programs;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+    private ListNode top;
+    private int lenght;
+    private class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+    public Stack(){
+        top = null;
+        lenght = 0;
+    }
+
+    public  int lenght(){
+        return lenght;
+    }
+    public boolean isEmpty(){
+        return lenght == 0;
+    }
+    public void push(int data){
+        ListNode temp = new ListNode(data);
+        temp.next = top;
+        top = temp;
+        lenght++;
+    }
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int result = top.data;
+        top = top.next;
+        lenght--;
+        return result;
+    }
+    public int peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        System.out.print(stack.peek()+"==>");
+        stack.pop();
+        System.out.print(stack.peek()+"==>");
+        stack.pop();
+        System.out.print(stack.peek());
+    }
+}
